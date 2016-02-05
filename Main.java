67fa7a383ecad8e534e3ad07fe358bd10589d2b1
Matt <deathbyman33@yahoo.com>
2016-02-05 12:05:41 -0800

    Small changes

--- Main.java
+++ Main.java
@@ -28,7 +28,7 @@ import java.util.Collections;
  * Created by Sphiinx on 12/26/2015.
  */
 @ScriptManifest(authors = "Sphiinx", category = "Cooking", name = "[SPX] AIO Cooker", version = 0.4)
-public class Main extends Script implements MessageListening07, Painting, MousePainting, MouseSplinePainting {
+public class Main extends Script implements MessageListening07, Painting {
 
     private Variables variables = new Variables();
     private ArrayList<Task> tasks = new ArrayList<>();
@@ -106,27 +106,7 @@ public class Main extends Script implements MessageListening07, Painting, MouseP
         }
     }
 
-    @Override
-    public void paintMouse(Graphics g, Point point, Point point1) {
-        g.setColor(scripts.SPXAIOPlanker.Constants.BLACK_COLOR);
-        g.drawRect(Mouse.getPos().x - 13, Mouse.getPos().y - 13, 27, 27); // Square rectangle Stroke
-        g.drawRect(Mouse.getPos().x, Mouse.getPos().y - 512, 1, 500); // Top y axis Line Stroke
-        g.drawRect(Mouse.getPos().x, Mouse.getPos().y + 13, 1, 500); // Bottom y axis Line Stroke
-        g.drawRect(Mouse.getPos().x + 13, Mouse.getPos().y, 800, 1); // Right x axis line Stroke
-        g.drawRect(Mouse.getPos().x - 812, Mouse.getPos().y, 800, 1); // left x axis line Stroke
-        g.fillOval(Mouse.getPos().x - 3, Mouse.getPos().y - 3, 7, 7); // Center dot stroke
-        g.setColor(scripts.SPXAIOPlanker.Constants.RED_COLOR);
-        g.drawRect(Mouse.getPos().x - 12, Mouse.getPos().y - 12, 25, 25); // Square rectangle
-        g.drawRect(Mouse.getPos().x, Mouse.getPos().y - 512, 0, 500); // Top y axis Line
-        g.drawRect(Mouse.getPos().x, Mouse.getPos().y + 13, 0, 500); // Bottom y axis Line
-        g.drawRect(Mouse.getPos().x + 13, Mouse.getPos().y, 800, 0); // Right x axis line
-        g.drawRect(Mouse.getPos().x - 812, Mouse.getPos().y, 800, 0); // left x axis line
-        g.fillOval(Mouse.getPos().x - 2, Mouse.getPos().y - 2, 5, 5); // Center dot
-    }
 
-    @Override
-    public void paintMouseSpline(Graphics graphics, ArrayList<Point> arrayList) {
-    }
 
     @Override
     public void playerMessageReceived(String s, String s1) {
